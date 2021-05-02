25f297a615f0ae70cd8a70ab21005bfc495f9181
jba11er32 <jonathan.1994.lee@gmail.com>
2021-05-01 19:30:41 -0700

    Random array of buttons lights up when play button is clicked

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// Select all the buttons and put in an array. We can then add functionality to each of these buttons
 const buttons = document.querySelectorAll('.button')
+console.log(buttons)
 
+// The play button should
 const playButton = document.querySelector('.play-button')
 
 function changeOpacity() {
@@ -9,7 +12,38 @@ function changeOpacity() {
     this.classList.add('lightUp')
 }
 
+
+
+
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', changeOpacity)
 }
 
+
+
+
+
+
+
+let randomArrayOfButtons = []
+
+
+playButton.addEventListener('click', () => {
+    
+    for (let i = 0; i < 10; i++) {
+        randomArrayOfButtons.push(`${buttons[Math.floor(Math.random() * buttons.length)].id}`)
+
+    }
+
+    for (let i = 0; i < 10; i++) {
+        // console.log(randomArrayOfButtons[i])
+        let button = document.getElementById(randomArrayOfButtons[i])
+        button.classList.remove('lightUp')
+        void button.offsetWidth
+        button.classList.add('lightUp')
+    }
+
+    
+    console.log(randomArrayOfButtons)
+    randomArrayOfButtons = []
+})
