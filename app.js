f2b1bbbc4988f4f8a458a50d2febffa3ed73071a
jba11er32 <jonathan.1994.lee@gmail.com>
2021-05-02 17:12:18 -0700

    Added some styling and javascript updates score and high score

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const playButton = document.querySelector('.play-button')
 
 const checkAnswer = document.querySelector('.check')
 
+const playerScore = document.querySelector('.js-player-score')
+
+const highScore = document.querySelector('.js-high-score')
+
+let randomArrayOfButtons = []
+
+let playerArray = []
+
+let score = 0;
+
+let highestScore = 0;
+
+
 
 
 function changeOpacity() {
@@ -14,10 +27,26 @@ function changeOpacity() {
     this.classList.add('lightUp')
 }
 
-
 function addColorToPlayerArray (event) {
     playerArray.push(event.target.id)
 }
+
+
+function addColorToRandomArray (arr) {
+    for (let i = 0; i < 1; i++) {
+        arr.push(`${buttons[Math.floor(Math.random() * buttons.length)].id}`)
+    }
+}
+
+function addScore() {
+    score++
+    if (score > highestScore) {
+        highestScore++
+    }
+}
+
+
+
 
 
 for (let i = 0; i < buttons.length; i++) {
@@ -27,34 +56,21 @@ for (let i = 0; i < buttons.length; i++) {
 
 
 
-function addColorToRandomArray (arr) {
-    for (let i = 0; i < 1; i++) {
-        arr.push(`${buttons[Math.floor(Math.random() * buttons.length)].id}`)
-    }
-}
-
-let randomArrayOfButtons = []
-
-
-let playerArray = []
-
-
-
 // https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
 // used this thread to set a delay to the for loop light up
-    const timer = ms => new Promise(res => setTimeout(res, ms))
+const timer = ms => new Promise(res => setTimeout(res, ms))
     
-    async function memoryArray (arr) {
-        for (let i = 0; i < arr.length; i++) {
-            // console.log(randomArrayOfButtons[i])
-            let button = this.document.getElementById(arr[i])
-            button.classList.remove('lightUp')
-            void button.offsetWidth
-            button.classList.add('lightUp')
-            // console.log(button)
-            await timer(1000)
-        }
+async function memoryArray (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(randomArrayOfButtons[i])
+        let button = this.document.getElementById(arr[i])
+        button.classList.remove('lightUp')
+        void button.offsetWidth
+        button.classList.add('lightUp')
+        // console.log(button)
+        await timer(1000)
     }
+}
 
 playButton.addEventListener('click', () => {
     playerArray = []
@@ -74,6 +90,8 @@ function compareArrays() {
     if (playerArray.length !== randomArrayOfButtons.length) {
         alert('Incorrect. Start Over!')
         randomArrayOfButtons = []
+        score = 0
+        playerScore.innerText = `Score: ${score}`
         return 
     }
 
@@ -81,13 +99,23 @@ function compareArrays() {
         if (playerArray[i] !== randomArrayOfButtons[i]) {
             alert('Incorrect. Start Over!')
             randomArrayOfButtons = []
+            score = 0
+            playerScore.innerText = `Score: ${score}`
             return
         }
     }
+
     alert('Congrats. Next Stage')
     addColorToRandomArray(randomArrayOfButtons)
     memoryArray(randomArrayOfButtons)
+    addScore()
+    playerScore.innerText = `Score: ${score}`
+    highScore.innerText = `Your High Score: ${highestScore}`
+    
+    
+    
     playerArray = []
+
 }
 
 
